e5635e334361642a93fafa5d74956724d907419f
Mirco Warnck <mirco.warnck@googlemail.com>
2022-02-08 10:51:05 +0100

    Create basic Room component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import './App.css';
+import Room from './Room.js';
 
 function App() {
-  return "Hello world";
+  return (
+    <main className="App">
+      <Room text="Küche" isClean />
+      <Room text="Wohnzimmer" isClean={false} />
+      <Room text="Bad" />
+    </main>
+  );
 }
 
 export default App;
